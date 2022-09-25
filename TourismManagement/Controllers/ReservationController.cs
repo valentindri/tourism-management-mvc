b708b9773c9e1276c7ac1db41093fe3b21b8fee9
Valentín Dri <51834435+valentindri@users.noreply.github.com>
2022-09-25 16:28:53 -0300

    Changed bootstrap theme

---
+++ TourismManagement/Controllers/ReservationController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TourismManagement.Controllers
+{
+    public class ReservationController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
